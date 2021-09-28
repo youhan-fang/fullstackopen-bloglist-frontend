7de94cac7ee3a44c6ef2b4ea13b51162ee2e192a
Youhan Fang <yfang@purdue.edu>
2021-09-28 12:08:36 -0700

    exercise 5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,43 @@
-import React from 'react';
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import React, { useState } from 'react';
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  };
+
+  const buttonStyle = {
+    backgroundColor: '#ffffff',
+    marginLeft: 8
+  };
+
+  const [visible, setVisible] = useState(false);
+  const buttonLabel = visible ? 'hide' : 'view';
+  const blogContent = () => {
+    return (
+      <div>
+        <div>{blog.url}</div>
+        <div>
+          likes {blog.likes}
+          <button style={buttonStyle}>like</button>
+        </div>
+        {blog.user ? <div>{blog.user.name}</div> : null}
+      </div>
+    );
+  };
+  console.log('blog content:', blog);
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button style={buttonStyle} onClick={() => {setVisible(!visible);}}>
+        {buttonLabel}
+      </button>
+      {visible ? blogContent() : null}
+    </div>
+  );
+};
 
 export default Blog;
